64ba5c5e0074393cdb20ae431ae08f059fab8be6
hussain758 <71380673+hussain758@users.noreply.github.com>
2024-02-06 11:48:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('get-data-button').addEventListener('click', function() {
+    const city = document.getElementById('city-select').value;
+    const apiKey = '6bcb2f15cb1c4dabb2981248240602'; 
+  
+
+
+
+});
+
